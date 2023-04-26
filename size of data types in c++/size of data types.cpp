499956c966edfdbc61b2dbdcff4041fccd218cb1
BhavinBairwa7764 <130303341+Bhavin7764@users.noreply.github.com>
2023-04-26 18:43:50 +0530

    Add files via upload

---
+++ size of data types in c++/size of data types.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"int = "<<sizeof(int);
+    cout<<"\n\nfloat = "<<sizeof(float);
+    cout<<"\n\ncharacter = "<<sizeof(char);
+    cout<<"\n\ndouble = "<<sizeof(double);
+    cout<<"\n\nboolean = "<<sizeof(bool);
+}
