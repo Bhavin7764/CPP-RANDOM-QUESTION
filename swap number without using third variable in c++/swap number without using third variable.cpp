8ae8a6fe6295719a68a8a4d676b5e1004a8dbdda
BhavinBairwa7764 <130303341+Bhavin7764@users.noreply.github.com>
2023-04-26 18:47:07 +0530

    Add files via upload

---
+++ swap number without using third variable in c++/swap number without using third variable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , b ;
+    cout<<"Enter a = ";cin>>a;
+    cout<<"\nEnter b = ";cin>>b;
+    a = a + b ;
+    b = a - b ;
+    a = a - b ;
+    cout<<"\na = "<<a;
+    cout<<"\nb = "<<b;
+}
