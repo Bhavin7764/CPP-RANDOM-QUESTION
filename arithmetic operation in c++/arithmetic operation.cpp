8eb29a8b1ea0db851ee81bc423542df6918ed17f
BhavinBairwa7764 <130303341+Bhavin7764@users.noreply.github.com>
2023-04-26 18:44:42 +0530

    Add files via upload

---
+++ arithmetic operation in c++/arithmetic operation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a , b ;
+    cout<<"Enter a = ";cin>>a;
+    cout<<"\nEnter b = ";cin>>b;
+    cout<<"\na + b = "<<a+b;
+    cout<<"\na - b = "<<a-b;
+    cout<<"\na / b = "<<a/b;
+    cout<<"\na * b = "<<a*b;
+    cout<<"\na % b = "<<a%b;
+
+}
